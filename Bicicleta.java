12920203bc79e092e60d3d46391ad767e6a6f11d
Ana Julia <88339570+AnaJuliaNX@users.noreply.github.com>
2024-04-01 21:51:25 -0300

    Primeiros passos com Java

---
+++ Bicicleta.java
@@ -0,0 +1,13 @@
+public class Bicicleta {
+    String guidao;
+    String cambio;
+    String roda;
+
+    public void trocaMarcha() {
+        System.out.println("código para trocar marcha");
+    }
+
+    public void pedalar() {
+        System.out.println("código para pedalar");
+    }
+}
